b7dfe2d8e3c60d4be83ca87bff453f8f0ee1b477
abdul wahab <63careful@gmail.com>
2021-03-30 09:16:49 +0000

    added snli with full grid search

--- Downstreamtask/SNLI/snliconfig.py
+++ Downstreamtask/SNLI/snliconfig.py
@@ -7,7 +7,7 @@ class snliConfig:
     vocab_size = 30522
     #seq_len = 128 #from hugging face
     seq_len = 128
-    batch_size = 128
+    batch_size = 64
     #batch_size = 32 #from hugging face
     epochs = 15
     lr_bert = 1e-5
--- Downstreamtask/SNLI/train_snli.py
+++ Downstreamtask/SNLI/train_snli.py
@@ -130,7 +130,7 @@ def train_val(train_data, valid_data, model_path:str,trial=None, best_params=Non
 
         if(trial is None and best_params is not None):
             print('-saving model-')
-            torch.save(model, 'results/full_text/dibert_SNLI_mlm_cls_pprediction_103_10_seed_'+str(4)+'_epoch_'+str(epoch+1)+'.tar')
+            torch.save(model, 'results/full_text_with_full_grid_search/dibert_SNLI_mlm_cls_pprediction_103_10_seed_'+str(4)+'_epoch_'+str(epoch+1)+'.tar')
 
     return valid_acc, score # tuning according to the last best validation accuracy
     #return sum(score.valid_acc)/len(score.valid_acc), score
@@ -164,7 +164,7 @@ def start_tuning(train_data, valid_data, model_path:str ,param_path:str, sampler
     return best_params, study.best_trial
 
 if __name__ == '__main__':
-    param_path = 'results/full_text/params/dibert_SNLI_mlm_cls_pprediction_10_best.json'
+    param_path = 'results/full_text_with_full_grid_search/params/dibert_SNLI_mlm_cls_pprediction_10_best.json'
     model_path = 'results/dibert_SNLI_mlm_cls_pp_29_seed_'+str(1)+'.tar'
 
     is_tune = False
